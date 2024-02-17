cb1285dc0d1bea40ad9912282335cf90a1f66cf3
Eric <eric@spiritsdontfly.com>
2024-02-17 15:04:32 +0100

    add postgres

--- app/api/[id]/route.js
+++ app/api/[id]/route.js
@@ -1,30 +1,45 @@
-import { notes } from "../notes.js"; /* sadly, the state does not persist across routes */
+import { sql } from "@vercel/postgres";
+
+export async function GET(request, { params }) {
+  const { id } = params;
+
+  const { rows } = await sql`SELECT * FROM notes WHERE id=${id}`;
+
+  if (!rows.length) {
+    return new Response(JSON.stringify({ error: "note not found" }), {
+      status: 404,
+    });
+  }
+
+  return new Response(JSON.stringify(rows[0]), { status: 200 });
+}
 
 export async function PUT(request, { params }) {
   const { content } = await request.json();
   const { id } = params;
 
-  if (!notes[id]) {
+  const { rowCount } =
+    await sql`UPDATE notes SET content = ${content} WHERE id=${id}`;
+
+  if (!rowCount) {
     return new Response(JSON.stringify({ error: "note not found" }), {
       status: 404,
     });
   }
 
-  notes[id] = { id, content };
-
-  return new Response(JSON.stringify(notes[id]), { status: 201 });
+  return new Response("Successfully edited the note.", { status: 201 });
 }
 
 export async function DELETE(request, { params }) {
   const { id } = params;
 
-  if (!notes[id]) {
+  const { rowCount } = await sql`DELETE FROM notes WHERE id=${id}`;
+
+  if (!rowCount) {
     return new Response(JSON.stringify({ error: "note not found" }), {
       status: 404,
     });
   }
 
-  delete notes[id];
-
-  return new Response("successfully deleted", { status: 204 });
+  return new Response("Successfully deleted the note.", { status: 200 });
 }
--- app/api/notes.js
+++
@@ -1,4 +0,0 @@
-export const notes = {
-  /* 1: { id: 1, content: "Hello, world!" }  */
-};
-export const nextId = { value: 1 };
